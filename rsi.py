ad4136995f6f1b8717bebb539866ddaf6766d766
emersondms <emersondms3@gmail.com>
2023-08-29 11:33:05 -0300

    Update rsi.py

--- rsi.py
+++ rsi.py
@@ -21,10 +21,11 @@ def calculate_rsi(prices_df, rsi_period=14):
     rsi = round(((100 - 100 / (1 + rs)).iloc[-1]), 2)
     return rsi
 
-def get_current_rsi_for_stock(mt5_conn, stock, timeframe, date_from, num_candles_lookback, rsi_period):
+def get_rsi_for_stock(mt5_conn, stock, timeframe, date_from, num_candles_lookback, rsi_period):
     rates = mt5_conn.copy_rates_from_pos(stock.strip(), timeframe, date_from, num_candles_lookback)
     rates_df = pd.DataFrame(rates)
-    current_rsi = calculate_rsi(rates_df['close'], rsi_period)
+    rsi = calculate_rsi(rates_df['close'], rsi_period)
+    return rsi
 
 
 
