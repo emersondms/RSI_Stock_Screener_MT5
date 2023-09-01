8581077079428f01a14cb047d2cd5a83eaf07469
emersondms <emersondms3@gmail.com>
2023-09-01 16:08:32 -0300

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import ttk
+
+class Window:
+    tree = None
+    refresh_button = None
+    
+    def __init__(self, root, df):
+        self.root = root
+        self.root.title("RSI Stock Screener")
+        self.root.resizable(False, False)
+        self.tree = ttk.Treeview(root)
+        
+        # Add a Scrollbar
+        self.scrollbar = ttk.Scrollbar(root, orient="vertical", command=self.tree.yview)
+        self.scrollbar.pack(side="right", fill="y")
+        self.tree.configure(yscrollcommand=self.scrollbar.set)
+
+        # Hide the Treeview's index column
+        self.tree["show"] = "headings"
+        self.tree.pack()
+
+        # Add a Refresh button
+        self.refresh_button = tk.Button(root, text="Refresh", command=self.insert_data(df))
+        self.refresh_button.pack()
+
+        self.insert_data(df)
+        
+    def insert_data(self, df):
+        self.tree.delete(*self.tree.get_children())
+  
+        # Convert DataFrame columns to numeric IDs
+        col_ids = range(len(df.columns))
+        self.tree["columns"] = col_ids
+
+        # Configure column headings
+        for col, col_id in zip(df.columns, col_ids):
+            self.tree.heading(col_id, text=col)
+            #self.tree.column(col_id, width=100) 
+
+        # Insert data from the DataFrame into the Treeview
+        for i, row in df.iterrows():
+            values = [row[col] for col in df.columns]
+            self.tree.insert("", i, values=values)
+
+  
